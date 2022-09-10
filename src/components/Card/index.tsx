87660a65327f7fe00909cab6e7f4a14bd3c08889
Jenifer Nagasaki <jenifer.nagasaki@hotmail.com>
2022-09-10 05:47:18 -0300

    chore: improved card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,22 +3,31 @@ import './index.css'
 
 export interface ICard {
   title: string
+  justifyContent?: boolean
   centerType?: 'none' | 'title' | 'all'
+  titleSize?: 'lg' | 'xl' | '2xl' | '3xl'
+  contentSize?: 'sm' | 'base' | 'lg'
   children?: React.ReactNode
 }
 
 const ICard: FC<ICard> = (props) => {
-  const titleStyle = 'text-3xl font-bold'+(props.centerType && props.centerType !== 'none' ? ' text-center' : '')
-  const contentStyle = 'mt-1 text-lg'+(props.centerType && props.centerType === 'all' ? ' text-center' : '')
+  const titleStyle = `text-${props.titleSize ?? '3xl'} font-bold ${
+    props.centerType && props.centerType !== 'none' ? 'text-center' : ''
+  }`
+  const contentStyle = `mt-1 text-${props.contentSize ?? 'lg'} ${
+    props.justifyContent
+      ? 'text-justify'
+      : props.centerType && props.centerType === 'all'
+      ? ' text-center'
+      : ''
+  }`
   return (
     <div className="flex rounded-md shadow-sm box-border-base">
       <div className="relative flex flex-grow items-stretch focus-within:z-10">
         <div className="flex w-full rounded-lg p-4 box-content">
           <div className="w-full">
             <h1 className={titleStyle}>{props.title}</h1>
-            <p className={contentStyle}>
-              {props.children}
-            </p>
+            <p className={contentStyle}>{props.children}</p>
           </div>
         </div>
       </div>
--- src/pages/Home/KiraNFTs/index.tsx
+++ src/pages/Home/KiraNFTs/index.tsx
@@ -3,12 +3,45 @@ import { FC, Fragment, useState } from 'react'
 import Button from '../../../components/Button'
 import InputButton from '../../../components/InputButton'
 import Card from '../../../components/Card'
+import ReactMarkdown from 'react-markdown'
 
 const KiraNFTs: FC = (props) => {
+  const content = `KIRA's genesis collection will consist of 10,001 NFTs that are
+  created by world-renowned artists Antoni and Marc Tudisco. KIRA offers 
+  ingeniously crafted alien-like humans delivered in 4K. Traits
+  are algorithmically generated to deliver similar but unique assets
+  to the consumer. There are common, rare, and one-of-a-kind properties and assets.
+  
+  Kira NFTs only carry artistic value and have no competitive impact on gameplay, but 
+  there are a wide variety of benefits to be unlocked by holding one.`
+
   const contentCards = [
-    { title: 'Battle Royale', content: 'Solos, Duos, Trios, Squads' },
-    { title: 'Elimination', content: '1v1 up to 10v10' },
-    { title: 'Elimination', content: '1v1 up to 10v10' }
+    {
+      title: 'Premium Access for Holders',
+      content: `If you hold a KIRA NFT you will unlock features such as: Staking to earn
+      $KIRA passively, NFT rental, free wager matches, free in-game skins,
+      purchase/eam all characters and weapons, and get the option to have
+      your NFT integrated as an in-game avatar. You'll also earn an additional
+      amount of SKIRA tokens for your gameplay compared to free players.`
+    },
+
+    {
+      title: 'Basic Access',
+      content: `Users who do not hold a KIRA NFT will only have
+      access to a limited quantity of characters, weapons,
+      skins, and upgrades, and will also earn less $KIRA
+      token for their gameplay compared to holder. Free
+      players will still have access to all game modes.`
+    },
+
+    {
+      title: 'Rent-A-KIRA',
+      content: `Our Kiraverse Marketplace will
+      feature an NFT rental section. This
+      system is created to allow users
+      the ability to place their NFTs up
+      for rent for other users to enjoy.`
+    }
   ]
 
   return (
@@ -22,22 +55,22 @@ const KiraNFTs: FC = (props) => {
             <br></br>
           </p>
           <hr></hr>
-          <p className="mt-4 text-lg text-xs leading-6 text-indigo-200">
-            KIRA's genesis collection will consist of 10,001 NFTs that are
-            created by world-re- nowned artists Antoni and Marc Tudisco. KIRA
-            Offers ingeniously crafted alien-like humans delivered in 4K. Traits
-            are algorithmically generated to deliver similar but unique assets
-            to the consumer. There are common, rare, and one-of-a-kind proper-
-            ties and assets.<br></br>Kira NFTs only carry artistic value and
-            have no competitive impact On gameplay, but there are a wide variety
-            of benefits to be unlocked by holding one.
+          <p className="mt-4 text-lg text-sm leading-6 text-indigo-200">
+            <ReactMarkdown>{content}</ReactMarkdown>
           </p>
         </div>
       </div>
 
       <div className="grid grid-cols-3 gap-4 mx-12 md:mx-36">
         {contentCards.map((item, index) => (
-          <Card key={index} title={item.title}>
+          <Card
+            key={index}
+            title={item.title}
+            centerType="all"
+            justifyContent
+            titleSize="xl"
+            contentSize="base"
+          >
             {item.content}
           </Card>
         ))}
